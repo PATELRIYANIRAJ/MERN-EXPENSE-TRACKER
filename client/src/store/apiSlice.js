9cc6266553692f09fb8bc0758d2fe8328f6e647d
PATELRIYANIRAJ <patelriya0451@gmail.com>
2022-10-30 16:15:57 +0530

    Initial Commit

---
+++ client/src/store/apiSlice.js
@@ -0,0 +1,34 @@
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+
+const baseURI= 'http://localhost:8080';
+export const apiSlice= createApi({
+    baseQuery: fetchBaseQuery({baseurl: baseURI }),
+    endpoints: builder => ({
+        getCategories:builder.query({
+            query:()=> '/api/categories'
+        }),
+
+        getLabels:builder.query({
+            query:()=>'/api/labels'
+        }),
+
+        addTransaction: builder.mutation({
+            query: (initialTransaction)=>({
+                url:'/api/transaction',
+                method:"POST",
+                body:initialTransaction
+            })
+        }),
+
+        deleteTransaction:builder.mutation({
+            query:recordId => ({
+                url:'/api/transaction',
+                method: "DELETE",
+                body:recordId
+            })
+        })
+
+    })
+})
+
+export default apiSlice;
